5ebcafcf510954f6f79127438f0fab1eb7f096cf
ramizakamala <mizarakamala@gmail.com>
2025-01-13 01:12:50 +0700

    menambahkan DeleteConfirmationDialog dengan AlertDialog untuk konfirmasi penghapusan data

--- app/src/main/java/com/example/firebase/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/firebase/ui/home/pages/HomeView.kt
@@ -96,3 +96,27 @@ fun HomeScreen(
     }
 }
 
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda Yakin Ingin Menghapus Data Ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
+
