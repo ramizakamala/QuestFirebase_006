cea92c43be28ae1a4f2aae4a2136c225d7517842
ramizakamala <mizarakamala@gmail.com>
2025-01-13 01:56:32 +0700

    menambahkan interface DestinasiNavigasi dan objek DestinasiHome serta DestinasiInsert untuk navigasi

---
+++ app/src/main/java/com/example/firebase/navigation/DestinasiNavigasi.kt
@@ -0,0 +1,15 @@
+package com.example.firebase.navigation
+
+interface DestinasiNavigasi {val route : String
+    val titleRes : String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
+
+object DestinasiInsert : DestinasiNavigasi {
+    override val route = "insert"
+    override val titleRes = "Insert"
+}
