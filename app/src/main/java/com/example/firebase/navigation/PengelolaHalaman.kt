cd12df9c8b5cd98eb45a765b3ccc76ea238749e3
ramizakamala <mizarakamala@gmail.com>
2025-01-13 02:06:19 +0700

    menambahkan navigasi antara Home dan Insert screen

--- app/src/main/java/com/example/firebase/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/firebase/navigation/PengelolaHalaman.kt
@@ -3,9 +3,14 @@ package com.example.firebase.navigation
 import android.os.Build
 import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.firebase.ui.home.pages.HomeScreen
-import java.lang.reflect.Modifier
+import com.example.firebase.ui.home.pages.InsertScreen
+
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
 @Composable
