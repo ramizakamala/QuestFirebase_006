5940db571c154f1078e891ff76f895ed7beccbc9
ramizakamala <mizarakamala@gmail.com>
2025-01-13 01:22:05 +0700

    menambahkan OnError untuk menampilkan pesan kesalahan dan tombol retry

--- app/src/main/java/com/example/firebase/ui/home/pages/HomeView.kt
+++ app/src/main/java/com/example/firebase/ui/home/pages/HomeView.kt
@@ -237,6 +237,28 @@ fun HomeStatus(
 fun OnLoading(modifier: Modifier = Modifier) {
     Text("Loading......")
 }
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    message: String
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Terjadi Kesalahan : $message",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = retryAction
+        ) {
+            Text("Retry")
+        }
+    }
+}
 
 
 
