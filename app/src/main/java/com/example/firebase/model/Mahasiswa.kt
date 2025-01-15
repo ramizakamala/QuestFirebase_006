1fd2aea7bec13c48991abb802b6fab0e112999d8
ramizakamala <mizarakamala@gmail.com>
2025-01-15 17:03:30 +0700

    tambah data class judulSkripsi dan Dosenpengampu

--- app/src/main/java/com/example/firebase/model/Mahasiswa.kt
+++ app/src/main/java/com/example/firebase/model/Mahasiswa.kt
@@ -6,8 +6,10 @@ data class Mahasiswa(
     val alamat: String,
     val jenisKelamin: String,
     val kelas: String,
-    val angkatan : String
+    val angkatan : String,
+    val judulSkripsi : String,
+    val Dosenpengampu : String
 ){
-    constructor() : this("", "", "", "", "", "")
+    constructor() : this("", "", "", "", "", "", "", "")
 }
 
